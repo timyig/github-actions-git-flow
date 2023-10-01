a25a9cbe4f4ce1da729715fc48b5765f22c2a95a
Timur Yigit <timur_yigit@outlook.com>
2023-10-01 11:01:25 +0000

    wip: testing run

--- act_run.sh
+++ act_run.sh
@@ -1,8 +1,11 @@
 # Sanity check
 echo "🧪🧪🧪 Sanity Check 🧪🧪🧪"
 act --list
-act --dryrun
-act --workflows .github/workflows/dev-pr.yml --actor timyig
+# Not running this in ACT as this is causing an error
+if [$ACT = false] ; then
+    act --dryrun
+fi
+printenv | grep ACT
 
 # Testing triggers
 echo "🧪🧪🧪 Testing Triggers 🧪🧪🧪"
@@ -10,3 +13,4 @@ echo "This should not trigger the workflow"
 act release --workflows .github/workflows/dev-pr.yml --actor timyig
 echo "This should trigger the workflow"
 act pull_request --workflows .github/workflows/dev-pr.yml --actor timyig
+act pull_request --workflows .github/workflows/dev-pr.yml --actor timyig --eventpath .github/workflows/act/event-dev-pr.json
