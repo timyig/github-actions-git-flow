20e920acf5f78619817fd30a575580d95b4e6e66
Timur Yigit <timur_yigit@outlook.com>
2023-10-01 09:04:22 +0000

    added some trigger tests

--- act_run.sh
+++ act_run.sh
@@ -1,2 +1,12 @@
+# Sanity check
+echo "🧪🧪🧪 Sanity Check 🧪🧪🧪"
 act --list
 act --dryrun
+act --workflows .github/workflows/dev-pr.yml --actor timyig
+
+# Testing triggers
+echo "🧪🧪🧪 Testing Triggers 🧪🧪🧪"
+echo "This should not trigger the workflow"
+act release --workflows .github/workflows/dev-pr.yml --actor timyig
+echo "This should trigger the workflow"
+act pull_request --workflows .github/workflows/dev-pr.yml --actor timyig
