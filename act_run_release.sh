4b468b9b0a780b81b1e3b6c56016a121e6a26d13
Timur Yigit <timur_yigit@outlook.com>
2023-10-04 12:45:05 +0000

    feat: added tests for retrieving version

--- act_run_release.sh
+++ act_run_release.sh
@@ -6,12 +6,25 @@ if [ "$ACT" = false ]; then
     act --dryrun
 fi
 
-act --workflows .github/workflows/release.yml --actor $GITHUB_USER
-
 # Testing triggers
 echo "🧪🧪🧪 Testing Triggers 🧪🧪🧪"
-echo "This should not trigger the workflow"
 echo "Github user var: ${GITHUB_USER}"
-act pull_request --workflows .github/workflows/release.yml --actor $GITHUB_USER --eventpath .github/workflows/act/event-release.json
-echo "This should trigger the workflow"
-act push --workflows .github/workflows/release.yml --actor $GITHUB_USER --eventpath .github/workflows/act/event-release.json
+echo "❓ Check if version is set"
+RESULT=$(act push --workflows .github/workflows/release.yml --actor $GITHUB_USER --eventpath .github/workflows/act/event-release.json  2>&1)
+# This test should not return an error
+if [[ ${RESULT,,} == *"failure"* ]]; then
+  echo "❌ Release workflow: The run failed"
+  echo -e "Result of run: \n ${RESULT}"
+else
+  echo "✅ Release workflow"
+fi
+
+echo "❓ Check if we catch no versions set"
+RESULT=$(act push --workflows .github/workflows/release.yml --actor $GITHUB_USER --eventpath .github/workflows/act/event-release-error.json  2>&1)
+# This should fail
+if [[ ${RESULT,,} == *"Error"* || ${RESULT,,} == *"failure"*  ]]; then
+  echo "✅ Release workflow successfully failed"
+else
+  echo "❌ Release workflow: The did not fail"
+  echo -e "Result of run: \n ${RESULT}"
+fi
