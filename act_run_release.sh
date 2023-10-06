b70bd3febe9e9280fc4b2dd7795225aa13a2d21a
Timur Yigit <timur_yigit@outlook.com>
2023-10-06 11:30:17 +0000

    wip: shell script refactored

--- act_run_release.sh
+++ act_run_release.sh
@@ -1,46 +1,65 @@
+#!/bin/bash
+# Test script for this worklfow:
+# .github/workflows/release.yml
+# when running this localy by triggering the workflow run this command:
+# act -W .github/workflows/act.yml --actor $GITHUB_USER --secret GITHUB_TOKEN=${GITHUB_TOKEN}
+# act push --workflows .github/workflows/release.yml --secret GITHUB_TOKEN=${GITHUB_TOKEN} --actor $GITHUB_USER --eventpath .github/workflows/act/event-release.json
+
 # Sanity check
 echo "🧪🧪🧪 Sanity Check 🧪🧪🧪"
-act --list
-# Not running this in ACT as this is causing an error
-if [ "$ACT" = false ]; then
-    act --dryrun
-fi
-
-# Testing triggers
-echo "🧪🧪🧪 Testing Triggers 🧪🧪🧪"
-echo "GITHUB Env variables"
- printenv | grep GITHUB_
-if [ -z "$GITHUB_TOKEN" ] && [ -z "$GH_TOKEN" ]; then
-  echo "❌ GITHUB_TOKEN is empty"
-  printenv | grep GITHUB_
+set -e
+if [ -z $GITHUB_TOKEN ] ; then
+  echo "GITHUB_TOKEN not set"
   exit 1
-else
-  echo "Found GITHUB_TOKEN"
 fi
 
-if [ -n "$GITHUB_TOKEN" ]; then
-  echo "Exporting GITHUB_TOKEN"
-  export GITHUB_TOKEN=$GITHUB_TOKEN
-else
-  echo "Exporting GH_TOKEN"
-  export GITHUB_TOKEN=$GH_TOKEN
+if [ -z $GITHUB_USER ] ; then
+  echo "GITHUB_USER not set"
+  exit 1
 fi
 
+# Not running this in ACT
+if [ -z "$ACT" ]; then
+  echo "Not running in act"
+else
+  echo "⚠️ Skipping steps when running in act"
+fi
+
+echo "🧪🧪🧪 Testing act --list 🧪🧪🧪"
+act --list
+act push --list
+act push --list --eventpath .github/workflows/act/event-release.json
+
+echo "🧪🧪🧪 Testing act --dryrun 🧪🧪🧪"
+act --workflows .github/workflows/release.yml --dryrun
+
+echo "🧪🧪🧪 Testing  triggers 🧪🧪🧪"
 echo "❓ Check if version is set"
-RESULT=$(act push --workflows .github/workflows/release.yml --secret GITHUB_TOKEN=${GITHUB_TOKEN} --actor $GITHUB_USER --eventpath .github/workflows/act/event-release.json  2>&1)
+export RESULT=$(act push \
+--workflows .github/workflows/release.yml \
+--secret GITHUB_TOKEN=${GITHUB_TOKEN} \
+--actor $GITHUB_USER \
+--eventpath .github/workflows/act/event-release.json 2>&1)
+
 # This test should not return an error
-if [[ ${RESULT,,} == *"failure"* ]]; then
+if [[ ${RESULT,,} == *"failure"* || ${RESULT,,} == *"error"* ]]; then
   echo "❌ Release workflow: The run failed"
   echo -e "Result of run: \n ${RESULT}"
   exit 1
 else
   echo "✅ Release workflow"
+  echo "$RESULT" | grep -o "Release version: [^ ]*"
 fi
 
-echo "❓ Check if we catch no versions set"
-RESULT=$(act push --workflows .github/workflows/release.yml --secret GITHUB_TOKEN=${GITHUB_TOKEN} --actor $GITHUB_USER --eventpath .github/workflows/act/event-release-error.json  2>&1)
+echo "❓ Check if we catch no version set"
+export RESULT=$(act push \
+--workflows .github/workflows/release.yml \
+--secret GITHUB_TOKEN=${GITHUB_TOKEN} \
+--actor $GITHUB_USER \
+--eventpath .github/workflows/act/event-release-error.json 2>&1)
+
 # This should fail
-if [[ ${RESULT,,} == *"Error"* || ${RESULT,,} == *"failure"*  ]]; then
+if [[ ${RESULT,,} == *"failure"* || ${RESULT,,} == *"error"* ]]; then
   echo "✅ Release workflow successfully failed"
 else
   echo "❌ Release workflow: The did not fail"
